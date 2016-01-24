50c7a709f948edfe129c285d8d77a7e77c4b6ecf
saganlives <astrec@gmail.com>
2016-01-24 11:41:27 -0800

    Base File

---
+++ WorldGen/WorldGen/Program.cs
@@ -0,0 +1,35 @@
+﻿//()===========================()
+//||    World Generator        ||
+//||                           ||
+//||                           ||
+//||                           ||
+//()===========================()
+//
+//
+//
+//
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of years to simulate: ");
+            int numYears = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numYears; i++)
+            {
+
+            }
+
+
+        }
+    }
+}
